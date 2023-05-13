d3ab6a68cf12d8b4e1a4d360f4db529e8eaef90d
Frank Claveria <frankrobertclaveria@gmail.com>
2023-05-13 02:11:16 -0500

    add 'use client' on navbar component

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">

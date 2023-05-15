e28eeb62d7980d008f3951a461b92306bf07e256
Frank Claveria <frankrobertclaveria@gmail.com>
2023-05-15 00:19:02 -0500

    add Register and Login UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
-import "./globals.css";
 import { Nunito } from "next/font/google";
-import Navbar from "./components/navbar/Navbar";
+
+import Navbar from "@/app/components/navbar/Navbar";
+import RegisterModal from "@/app/components/modals/RegisterModal";
+
+import ToasterProvider from "@/app/providers/ToasterProvider";
+
+import "./globals.css";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -17,6 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>

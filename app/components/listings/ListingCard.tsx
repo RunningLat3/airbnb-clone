37fb3d7fbd9f6085ee1600df69f9cbb1386ccd67
Frank Claveria <frankrobertclaveria@gmail.com>
2023-05-27 20:43:32 -0500

    add individual listing view

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
 
 import useCountries from "@/app/hooks/useCountry";
 import { useRouter } from "next/navigation";
@@ -11,7 +11,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
